9d948a604557c0e04c810abf7a7b889f79e2276b
bimarsha7 <bmrsbhandari@gmail.com>
2019-06-26 15:33:39 +0545

    url for movies configured and routed

--- moviedb/imdb/views.py
+++ moviedb/imdb/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import ListView
-from models import Movie
+from imdb.models import Movie
 
 class movie_list(ListView):
     model = Movie
