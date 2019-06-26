fbc5bb7fd8970b1de9e6dc305e04b32e06638a24
bimarsha7 <bmrsbhandari@gmail.com>
2019-06-26 19:00:46 +0545

    base html , movie detail urls updated

--- moviedb/imdb/urls.py
+++ moviedb/imdb/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'imdb'
 urlpatterns = [
-    path('movies', views.movie_list.as_view(), name = 'movieList'), 
+    path('movies', views.movie_list.as_view(), name = 'MovieList'),
+    path('movie/<int:pk>', views.movie_detail.as_view(), name = 'MovieDetail')
 ]
