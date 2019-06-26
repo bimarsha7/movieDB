15126a2e292fe1b742e55140c97cc278eb3a94cf
bimarsha7 <bmrsbhandari@gmail.com>
2019-06-26 17:11:54 +0545

    template direcotry asigned in settings

--- moviedb/imdb/views.py
+++ moviedb/imdb/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from imdb.models import Movie
 
 class movie_list(ListView):
     model = Movie
 
+class movie_detail(DetailView):
+    model = Movie
