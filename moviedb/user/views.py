f05bb77db19e4d612b45fb7c20384f4c06fbd846
bimarsha7 <bmrsbhandari@gmail.com>
2019-06-27 10:01:12 +0545

    register view

--- moviedb/user/views.py
+++ moviedb/user/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
-from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 
@@ -13,6 +13,4 @@ class RegisterView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('imdb:MovieList')
 
-@login_required
-def dashboard(request):
-    return render(request,'imdb/movie_list.html',{'section': 'dashboard'})
+
