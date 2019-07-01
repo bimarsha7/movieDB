63bcff9c4b08e764cdee965fff4cc0104b70b375
bimarsha7 <bmrsbhandari@gmail.com>
2019-07-01 09:02:58 +0545

    function name sign up corrected

--- moviedb/user/forms.py
+++ moviedb/user/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-class sing_up_form(UserCreationForm):
+class sign_up_form(UserCreationForm):
     email = forms.EmailField(label="Email", max_length=150)
 
     class Meta:
