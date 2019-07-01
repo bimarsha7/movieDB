1a5d916a1a25ce329176113421d4ff015f3fd162
bimarsha7 <bmrsbhandari@gmail.com>
2019-07-01 09:04:08 +0545

    sign up form corrected and imported

--- moviedb/user/views.py
+++ moviedb/user/views.py
@@ -6,18 +6,18 @@ from django.shortcuts import render, redirect
 # from django.views.generic import CreateView
 
 from django.contrib import messages
-from .forms import sing_up_form
+from .forms import sign_up_form
 
 def signup(request):
     if request.method == 'POST':
-        form = sing_up_form(request.POST)
+        form = sign_up_form(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('imdb:MovieList')
     else:
-        form = sing_up_form()
+        form = sign_up_form()
     return render(request, 'user/register.html', {'form': form})
 
 # class RegisterView(CreateView):
