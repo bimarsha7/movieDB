a415aa814cd699303bfb1fb00532d2f587a3c9a2
bimarsha7 <bmrsbhandari@gmail.com>
2019-07-02 16:01:11 +0545

    signUp and auto login if user ia active

--- moviedb/user/urls.py
+++ moviedb/user/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-
+from django.conf.urls import url
 from user import views as user_views
 
 app_name = 'user'
 urlpatterns = [
-    path('register/', user_views.register, name='register'),
+    path('signup/', user_views.signup, name='register'),
+    # url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    #     user_views.activate, name='activate'),
+    path('activate/<slug:uidb64>/<slug:token>', user_views.activate, name='activate'),
 ]
