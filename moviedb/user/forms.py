9bb38b628f9267d91220d6ca3b3438738bae5e0a
bimarsha7 <bmrsbhandari@gmail.com>
2019-07-12 23:26:46 +0545

    duplicate email and username error

--- moviedb/user/forms.py
+++ moviedb/user/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 class sign_up_form(UserCreationForm):
     email = forms.EmailField(label="Email", max_length=150)
@@ -9,3 +10,16 @@ class sign_up_form(UserCreationForm):
         model = User
         fields = ['email', 'username', 'password1', 'password2']
 
+    def clean_email(self):
+        email = self.cleaned_data['email'].lower()
+        r = User.objects.filter(email=email)
+        if r.count():
+            raise ValidationError('Email already exist. Check your Email and Try again.')
+        return email
+    
+    def clean_username(self):
+        username  = self.cleaned_data['username'].lower()
+        r = User.objects.filter(username = username)
+        if r.count():
+            raise ValidationError('Username already exist. Try another one')
+        return username
