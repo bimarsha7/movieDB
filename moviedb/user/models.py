9a565639f63d2657f257d69c1023d94a43acc641
bimarsha7 <bmrsbhandari@gmail.com>
2019-07-28 18:14:34 +0545

    user_profile model created in user app

--- moviedb/user/models.py
+++ moviedb/user/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+# The user 'one-to-one' field allows us to associate profiles with
+# users. We use CASCADE for the 'on_delete' parameter so that its related
+# profile also gets deleted when a user is deleted. The photo field is an
+# ImageField field. We will need to install the Pillow library to handle
+# images.
+class user_profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to = 'user/%Y/%m/%d/', blank=True)
+
+    def __str__(self):
+        return 'Profile for user {}'.format(self.user.username)
+
+
+
