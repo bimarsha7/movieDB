20dcb38619bd022b7b3179231b25318c553a807e
bimarsha7 <bmrsbhandari@gmail.com>
2019-07-28 18:14:08 +0545

    user edit form nd profile edit form created

--- moviedb/user/forms.py
+++ moviedb/user/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
+from .models import user_profile
 
 class sign_up_form(UserCreationForm):
     email = forms.EmailField(label="Email", max_length=150)
@@ -27,3 +28,19 @@ class sign_up_form(UserCreationForm):
 class login_form(forms.Form):
     username = forms.CharField(label="Username", max_length=150)
     password = forms.CharField(label="Password" , widget = forms.PasswordInput)
+
+# 'user_edit_form()': This will allow users to edit their first name, last
+# name, and email, which are attributes of the built-in Django
+# user model
+class user_edit_form(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+# Users will be able to edit
+# their date of birth and upload a picture for their profile.
+class profile_edit_form(forms.ModelForm):
+    class Meta:
+        model = user_profile
+        fields = ('date_of_birth', 'photo')
+    
