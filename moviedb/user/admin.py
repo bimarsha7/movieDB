7733065a756edb43a5c97761e43030b127d4b242
bimarsha7 <bmrsbhandari@gmail.com>
2019-07-29 13:08:08 +0545

    user_profile renamed

--- moviedb/user/admin.py
+++ moviedb/user/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import user_profile
+from .models import Profile
 
 # Register your models here.
-@admin.register(user_profile)
+@admin.register(Profile)
 class profile_admin(admin.ModelAdmin):
     list_display = ['user', 'date_of_birth', 'photo']
     
