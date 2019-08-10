8e2106c1a4d6a8225a78dfde411b59fc5208857a
bimarsha7 <bmrsbhandari@gmail.com>
2019-08-10 17:43:54 +0545

    model created

--- moviedb/imdb/models.py
+++ moviedb/imdb/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Movie(models.Model):
@@ -24,6 +25,25 @@ class Movie(models.Model):
 
     def __str__(self):
         return '{} ({})'.format(self.title, self.year)
-    
+# ==========================================================================================================
+# creating a vote-model to allow different users to vote movie they love or hate
+class Vote(models.Model):
+    UP = 1
+    DOWN = -1
+    VALUE_CHOICES = (
+        ('UP', 'u'),
+        ('DOWN', 'd'),
+    )
+    value = models.SmallIntegerField(choices= VALUE_CHOICES)
+    # add custom user model not AUTH_USER_MODEL
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete= models.CASCADE)
+    voted_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+    # 'unique_together' attribute of Meta creates a unique constraint on the table. A unique
+    # constraint will prevent two rows having the same value for both 'user' and 'movie', enforcing
+    # our rule of one vote per user per movie.
+        unique_together = ('user', 'movie')
 
 
