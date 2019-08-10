4270f1f67ff4b47f82de8729f6b9f9e95f0692df
bimarsha7 <bmrsbhandari@gmail.com>
2019-08-10 16:58:02 +0545

    add logout path in url

--- moviedb/user/urls.py
+++ moviedb/user/urls.py
@@ -10,6 +10,7 @@ app_name = 'user'
 urlpatterns = [
     path('signup/', user_views.user_signup, name='signup'),
     path('login/', login_views.as_view(), name='login'),
+    path('logout/', user_views.user_logout, name='logout'),
     # url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
     #     user_views.activate, name='activate'),
     path('activate/<slug:uidb64>/<slug:token>', user_views.activate, name='activate'),
